12a81bb8fb599d412365b1ae7965edd5b9f7e2c0
Kyle M Hall <kyle@bywatersolutions.com>
2019-10-02 14:51:05 +0000

    Fix up Host netmask for db

--- ansible/files/docker-entrypoint-initdb.d/db_on_init.sql
+++ ansible/files/docker-entrypoint-initdb.d/db_on_init.sql
@@ -1,3 +1,3 @@
 --DELETE FROM mysql.user WHERE Host = 'localhost';
-UPDATE mysql.user SET Host = '172.17.%.%' WHERE User = 'root';
+UPDATE mysql.user SET Host = '172.%.%.%' WHERE User = 'root';
 FLUSH PRIVILEGES;
