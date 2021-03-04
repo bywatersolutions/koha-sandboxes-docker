966370a499ebe3fb688a8a6085d6db5bd7bf9428
root <root@sandboxes.development.ptfsadmin.uk0.bigv.io>
2021-03-04 14:42:09 +0000

    Update to use python3

--- setup-sandbox-server.sh
+++ setup-sandbox-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i "localhost," -c local ansible/setup-sandbox-server.yml -v
+ansible-playbook -i "localhost," -c local ansible/setup-sandbox-server.yml -v -e 'ansible_python_interpreter=/usr/bin/python3.7'
